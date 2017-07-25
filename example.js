dc27b800b00fc9e8f75a62c2121f0d99680745b7
alanhett <ahettinger@gmail.com>
2017-07-24 23:01:03 -0400

    updated readme

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ var ace = require('./ace');
 
 // encoding a string
 s = ace.encode('Hello world!');
-console.log(s); // osswununuserveusuwunsves
+console.log(s); // sosswununuserveusuwunsves
 
 // decoding a string
 s = ace.decode(s);
@@ -10,15 +10,15 @@ console.log(s); // Hello world!
 
 // encoding an integer
 i = ace.encode(125038937);
-console.log(i); // xczzosacw
+console.log(i); // xczxvzrnw
 
 // decoding an integer
 i = ace.decode(i);
-console.log(i); // 125283948
+console.log(i); // 125038937
 
 // encoding a float
 f = ace.encode(123.5);
-console.log(f); // mwmxmzmsnc
+console.log(f); // smwmxmzmsnc
 
 // decoding a float
 f = ace.decode(f);
