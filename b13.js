f06c38b98c6f5cfbcb05ff9a41e618f1af25dd3e
uwx <chris331hansen@outlook.com>
2017-08-15 00:04:42 -0300

    fix number support + add support for encoding raw byte streams

--- b13.js
+++ b13.js
@@ -18,6 +18,36 @@
 
 const {keys} = Object;
 
+function arrayBufferToBase64(buffer) {
+  var binary = '';
+  var bytes = new Uint8Array(buffer);
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
+}
+
+function base64ToArrayBuffer(base64) {
+  var binaryString = window.atob(base64);
+  var len = binaryString.length;
+  var bytes = new Uint8Array(len);
+  for (var i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
+function base64ToArray(base64) {
+  var binaryString = window.atob(base64);
+  var len = binaryString.length;
+  var bytes = [];
+  for (var i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes;
+}
+
 var charset = {
   'q': ['w'],
   'e': ['r'],
@@ -61,75 +91,53 @@ function decodeIndex(x) {
 
 // flags
 // s - string
-// x - integer
-// z - negative integer
-// o - float
-// r - negative float
-// w - float backfill
+// b - Number
+// y - array (implicit, so array-like and Uint8Array works as well)
 
-// TODO add support for encoding raw byte streams
 function encode(s) {
-  if (typeof(s) == 'string') return encodeString(s);
-  if (s.toString().replace('.', '').length > 15) {
-    console.warn('Warning: Numbers longer than 15 digits may lose precision when encoded.');
-  }
-  if (typeof(s) == 'number' && s % 1 === 0) return encodeInteger(s);
-  if (typeof(s) == 'number') return encodeFloat(s);
+  if (isNaN(s)) return 'nan'; // specify test for NaN since NaN does not equal itself
+  if (!s) return ''; // if no `s` provided, or s == ''
+  if (typeof(s) == 'string') return 's' + encodeString(s);
+  if (typeof(s) == 'number') return 'b' + encodeNumber(s);
+  if (s.length !== undefined) return 'y' + encodeRaw(s);
 }
 
 function decode(s) {
   if (!s) return ''; // if no `s` provided, or s == ''
-  if (s[0] == 's') return decodeString(s);
-  if (s[0] == 'x' || s[0] == 'z') return decodeInteger(s);
-  if (s[0] == 'o' || s[0] == 'r') return decodeFloat(s);
+  if (s == 'nan') return NaN;
+  if (s[0] == 's') return decodeString(s.substring(1));
+  if (s[0] == 'b') return decodeNumber(s.substring(1));
+  if (s[0] == 'y') return decodeRaw(s.substring(1));
 }
 
-function encodeInteger(s) {
-  let flag = (s >= 0) ? 'x' : 'z';
-  s = Math.abs(s).toString(13);
-  return flag + s.split('').map(i => { return a[parseInt(i, 13)] }).join('');
+function encodeNumber(s) {
+  return encodeString(s.toString());
 }
 
 function encodeString(s) {
-  return 's' + s.split('').map(i => {
+  return s.split('').map(i => {
     i = i.charCodeAt(0).toString(13);
     return a[parseInt(i[0], 13)] + a[parseInt(i[1], 13)];
   }).join('');
 }
 
-function encodeFloat(s) { // FIXME this is broken
-  let flag = (s >= 0) ? 'o' : 'r';
-  s = Math.abs(s).toString();
-  let d = s.indexOf('.').toString(13);
-  d = d.split('').map(i => { return a[parseInt(i, 13)] }).join('');
-  d = (d.length < 2) ? 'w' + d : d;
-  s = parseInt(s.replace('.', '')).toString(13);
-  s = s.split('').map(i => { return a[parseInt(i, 13)] }).join('');
-  return flag + d + s;
-}
-
-function decodeInteger(s) {
-  let sign = (s[0] == 'x') ? 1 : -1;
-  s = s.slice(1);
-  s = s.split('').map(i => { return decodeIndex(i).toString(13) }).join('');
-  return sign * parseInt(s, 13);
+function encodeRaw(s) {
+  return encodeString(arrayBufferToBase64(s));
 }
 
 function decodeString(s) {
-  return s.slice(1).match(/.{2}/g).map(i => {
+  return s.match(/.{2}/g).map(i => {
     i = decodeIndex(i[0]).toString(13) + decodeIndex(i[1]).toString(13);
     return String.fromCharCode(parseInt(i, 13));
   }).join('');
 }
 
-function decodeFloat(s) {
-  let sign = (s[0] == 'o') ? 1 : -1;
-  let d = (s[1] == 'w') ? s.slice(2, 3) : s.slice(1, 3);
-  d = d.split('').map(i => { return decodeIndex(i).toString(13) }).join('');
-  d = parseInt(d, 13);
-  s = s.slice(3);
-  s = s.split('').map(i => { return decodeIndex(i).toString(13) }).join('');
-  return sign * parseInt(s, 13) / Math.pow(10, s.length - d + 1);
+function decodeNumber(s) {
+  return parseFloat(decodeString(s));
+}
+
+function decodeRaw(s) {
+  return base64ToArray(decodeString(s));
 }
 
 // detect browser/nodejs
